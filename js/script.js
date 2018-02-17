8845df2b6bc5a8402b777f27d0d69aee96ceddd1
Rafal Swieton <rafal@eskrzynka.pl>
2018-02-18 00:35:42 +0100

    Change index.html, script.js

--- js/script.js
+++ js/script.js
@@ -1,1 +1,13 @@
 // script.js
+
+var list = document.getElementById('list'),
+	add = document.getElementById('addElem');
+
+add.addEventListener('click', function(e) {
+	var element = document.createElement('li'),
+		itemsByTagName = document.getElementsByTagName('li');
+		
+	element.innerHTML = 'item ' + itemsByTagName.length;
+	
+	list.appendChild(element);
+});
